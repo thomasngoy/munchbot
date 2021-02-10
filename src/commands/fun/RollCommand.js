b51aed69418e3ab00f79a1ef2a8a6bf53e56ffb2
thomasngoy <56560467+thomasngoy@users.noreply.github.com>
2021-02-10 00:16:48 -0800

    Update RollCommand.js

--- src/commands/fun/RollCommand.js
+++ src/commands/fun/RollCommand.js
@@ -10,12 +10,24 @@ module.exports = class RollCommand extends BaseCommand {
   run(client, message, args) {
     var x = Math.floor(Math.random()*6) + 1;
 
-    var diceimg = "https://i.imgur.com/s39XF8h.jpg"
+    var dice1 = "https://i.imgur.com/ZQzlpw6.gif";
+    
+    if(x == 2){
+      dice1 = "https://i.imgur.com/QsmwQui.gif";
+    } else if (x == 3){
+      dice1 = "https://i.imgur.com/e6KGcxJ.gif";
+    } else if (x == 4){
+      dice1 = "https://i.imgur.com/tngZupv.gif";
+    } else if (x == 5){
+      dice1 = "https://i.imgur.com/kl3N8qh.gif";
+    } else if (x == 6){
+      dice1 = "https://i.imgur.com/KPQ7JYy.gif";
+    }
 
     const sayEmbed = new Discord.MessageEmbed()
       .setTitle(message.author.username + " has rolled a " + x)
       .setFooter(message.author.tag , message.author.displayAvatarURL())
-      .setImage(diceimg)
+      .setImage(dice1)
       .setColor(" #ECAD2A")
       .setTimestamp();
 
